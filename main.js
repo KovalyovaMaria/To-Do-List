9dfe18caf7125ac047b1a838db80bb684c11cdd5
KovalyovaMaria <idKovalyova@gmail.com>
2024-07-01 15:55:42 +0300

    git commit -m  "first commit"

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ let taskNameInput = document.querySelector("#task-name-input");
 let addTaskBtn = document.querySelector("#add-task-btn");
 let startMessage = document.querySelector("#start-message");
 let taskList = document.querySelector(".task-list");
+let tasks = [];
+
 
 addTaskBtn.addEventListener("click", addTaskHandler);
 taskList.addEventListener("click", changeTaskState);
 
-function createTask(text) {
+
+        function createTask(text) {
 let div = document.createElement("div");
             div.classList.add("task");
 
@@ -15,6 +18,7 @@ let div = document.createElement("div");
 
             let p = document.createElement("p");
             p.innerText = text;
+
 
             let btn = document.createElement("button");
             btn.addEventListener("click",() => {
@@ -41,8 +45,6 @@ let div = document.createElement("div");
             }
         }
 
-
-
         function addTaskHandler() {
             if (taskNameInput.value) {
                 if(!startMessage.hidden)startMessage.hidden = true;
@@ -50,7 +52,16 @@ let div = document.createElement("div");
                 let newTask = createTask(taskNameInput.value);
                 taskList.append(newTask);
                 taskNameInput.value = "";
-            } else {
+
+            }else {
                 alert("Ваше поле пусте. Введіть завдання");
             }
         }
+
+    function filterTasks() {
+    const activeTasks = tasks.filter(element => element.completed === false);
+    const completedTasks =  tasks.filter(element => element.completed = true);
+    tasks = [activeTasks ,completedTasks ];
+}
+
+filterTasks();
